f0f120d7d2827d629816e4f095c7c1381f7279be
Sharon <hoffmanqt@gmail.com>
2025-04-01 12:19:36 +0300

    secont commit

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @echo off
 call venv\scripts\activate
-pytest -v -s .\test_cases\test_admin_login.py --html reports/report.html
+pytest -v -s -m .\test_cases\test_admin_login.py --html reports/report.html
